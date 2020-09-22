360697cbf5ee382ab34ca949776346c80111a402
EROS0820 <eros0820@protonmail.com>
2020-09-22 04:33:24 +0300

    added: file upload feature

--- Python_PSD_Edit/process/views.py
+++ Python_PSD_Edit/process/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-
+from django.core.files.storage import FileSystemStorage
 # Create your views here.
 
 def dashboard(request):
 	return render(request, 'dashboard.html')
+
+def upload(request):
+    context = {}
+    if request.method == 'POST':
+        leftsleev = request.FILES['leftsleev']
+        rightsleev = request.FILES['rightsleev']
+        lefthood = request.FILES['lefthood']
+        righthood = request.FILES['righthood']
+        front = request.FILES['front']
+        fs = FileSystemStorage()
+        name = fs.save('leftsleev', leftsleev)
+        name = fs.save('rightsleev', rightsleev)
+        name = fs.save('lefthood', lefthood)
+        name = fs.save('righthood', righthood)
+        name = fs.save('front', front)
+        # context['url'] = fs.url(name)
+    return render(request, 'dashboard.html', context)
+
